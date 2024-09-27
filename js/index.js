e879c1695e757583fc5732c62fdf67aaf695efdc
baralchirag <baralchirag23@gmail.com>
2024-09-27 20:36:58 +0545

    sound

--- js/index.js
+++ js/index.js
@@ -11,6 +11,11 @@ const modalEl = document.querySelector('#modalEl');
 const bigScore = document.querySelector('#bigScore');
 const c = canvas.getContext('2d');
 
+
+const CollisionSound= new Audio('./sound/gameOver.mp3');
+const Shooting= new Audio('./sound/shooting.mp3');
+const killEnemy= new Audio('./sound/kill.mp3');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -79,6 +84,8 @@ function animate() {
         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
 
         if (dist - enemy.radius - player.radius < 1) {
+            CollisionSound.currentTime = 0; 
+            CollisionSound.play();
             cancelAnimationFrame(animationId);
             modalEl.style.display = 'flex';
             bigScore.innerHTML = score;
@@ -88,6 +95,8 @@ function animate() {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
             if (dist - enemy.radius - projectile.radius < 1) {
                 for (let i = 0; i < enemy.radius * 2; i++) {
+                    killEnemy.currentTime = 0; // Reset sound
+                    killEnemy.play();
                     particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
                         x: (Math.random() - 0.5) * (Math.random() * 6),
                         y: (Math.random() - 0.5) * (Math.random() * 6),
@@ -116,6 +125,8 @@ function animate() {
 }
 
 window.addEventListener('click', (event) => {
+    Shooting.currentTime = 0;
+    Shooting.play();
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = { x: Math.cos(angle) * 5, y: Math.sin(angle) * 5 };
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity));
